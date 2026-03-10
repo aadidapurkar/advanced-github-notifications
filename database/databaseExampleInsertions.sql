86d1cee2f79bb733dd742093bcbb5dfac4546b26
Aadi Dapurkar <aadidapurkar04@gmail.com>
2026-03-11 02:27:03 +1100

    get unique subscriptions + minor example db data update

--- database/databaseExampleInsertions.sql
+++ database/databaseExampleInsertions.sql
@@ -6,10 +6,19 @@ INSERT INTO USERS(username, email, browserNotifPushURL, discordWebhookURL, encry
     "38c30a42a13f13dd114c733296bea1ef:4c96100a247b2148e7087ebf6e6271f88058132aca7c6bfe42ee4b92c36ec7f5ca0865ffa4b352be0a5a1be0402a5249"
 );
 
+INSERT INTO USERS(username, email, browserNotifPushURL, discordWebhookURL, encryptedPAT) VALUES (
+    "user2",
+    "user2@gmail.com",
+    "http://browsernotifpushurl.testdomain",
+    "http://discordpushurl.test.domain",
+    "38c30a42a13f13dd114c733296bea1ef:4c96100a247b2148e7087ebf6e6271f88058132aca7c6bfe42ee4b92c36ec7f5ca0865ffa4b352be0a5a1be0402a5249"
+);
+
 INSERT INTO SUBSCRIPTIONS (subscriber, username, repo) VALUES 
     (1, "aadidapurkar", "test-notif"),
     (1, "aadidapurkar", "lockin"),
-    (1, "microsoft", "vscode");
+    (1, "microsoft", "vscode"),
+    (2, "aadidapurkar", "test-notif");
 
 INSERT INTO EVENTS_SUBSCRIPTIONS (subscriptionRef, eventType) VALUES 
         (1, "CreateEvent"),
--- github-rest-api/atomicGithubFunctions.ts
+++ github-rest-api/atomicGithubFunctions.ts
@@ -8,6 +8,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { GithubRepoEvent } from "../types";
 import { updateSubscription } from "../database/safeQueries";
+import { getSubscriptions } from "../database/safeQueries";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
@@ -15,7 +16,7 @@ const cachedFetch = makeFetchHappen.defaults({
   cachePath: "./.gh-cache",
 });
 import { Result } from "../util";
-
+import { Subscription } from "../server/zod-schemas";
 
 // octokit auth for 5500 request/hour authenticated rate limit as opposed to 60
 const octokit = new Octokit({
@@ -26,7 +27,7 @@ const octokit = new Octokit({
 });
 
 
-
+// Get github events
 export const getRecentGithubEventsForSubscription = async (
   owner: string,
   repo: string
@@ -45,3 +46,44 @@ export const getRecentGithubEventsForSubscription = async (
 };
 
 // console.log(await getRecentGithubEventsForSubscription("microsoft", "vscode")); // test above fn
+
+// Filter for unparsed events
+export const getUnparsedEvents = (
+  es: GithubRepoEvent[], 
+  latestEventTime: Date
+) : GithubRepoEvent[] => {
+  return es.filter(
+    (e) => {
+      const d = new Date(e.created_at ? e.created_at : "1900-01-01")
+      return d > latestEventTime
+    }
+  )
+}
+
+// const testRes = await getRecentGithubEventsForSubscription("aadidapurkar", "test-notif");
+// console.log(await getUnparsedEvents(testRes[1]!, new Date("2026-03-05")).length)
+
+
+// Get all UNIQUE subscriptions, returns an object where keys are string username-repo, values are an array of two sized tuples where fst represents user id, snd represents subscription id of user
+// Used to prevent redundant GithubEvent requests
+const getUniqueSubscriptions = async () : Promise<Result<{ [key: string]: [number, number][] }>> => {
+  const [err, subs] = await getSubscriptions();
+  if (err) {
+    return [err, null]
+  }
+  let seenSubscriptions : { [key: string]: [number, number][] } = {}
+
+  subs.map((s) => {
+    const k = `${s.username!}-${s.repo!}`
+    if (k in seenSubscriptions) {
+      seenSubscriptions[k].push([s.subscriber!, s.id!])
+    } else {
+      seenSubscriptions[k] = [[s.subscriber!, s.id!]]
+    }
+  })
+
+  return [null, seenSubscriptions]
+
+}
+
+console.log(JSON.stringify(await getUniqueSubscriptions(), null));
