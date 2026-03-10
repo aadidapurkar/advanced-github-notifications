d66a644b67aba9d0819a5e7b311de3bda977a776
Aadi Dapurkar <aadidapurkar04@gmail.com>
2026-03-10 21:52:50 +1100

    updated example data insertions

--- database/databaseExampleInsertions.sql
+++ database/databaseExampleInsertions.sql
@@ -1,31 +1,29 @@
+INSERT INTO USERS(username, email, browserNotifPushURL, discordWebhookURL, encryptedPAT) VALUES (
+    "aadidapurkar",
+    "aadidapurkar04@gmail.com",
+    "http://browsernotifpushurl.testdomain",
+    "http://discordpushurl.test.domain",
+    "38c30a42a13f13dd114c733296bea1ef:4c96100a247b2148e7087ebf6e6271f88058132aca7c6bfe42ee4b92c36ec7f5ca0865ffa4b352be0a5a1be0402a5249"
+);
 
+INSERT INTO SUBSCRIPTIONS (subscriber, username, repo) VALUES 
+    (1, "aadidapurkar", "test-notif"),
+    (1, "aadidapurkar", "lockin"),
+    (1, "microsoft", "vscode");
 
+INSERT INTO EVENTS_SUBSCRIPTIONS (subscriptionRef, eventType) VALUES 
+        (1, "CreateEvent"),
+        (1, "ReleaseEvent"),
+        (1, "IssuesEvent"),
 
+        (2, "CreateEvent"),
+        (2, "ReleaseEvent"),
+        (2, "IssuesEvent"),
 
+        (3, "CreateEvent"),
+        (3, "ReleaseEvent"),
+        (3, "IssuesEvent"),
+        (3, "GollumEvent"),
+        (3, "ReleaseEvent"),
+        (3, "IssuesEvent"),
 
--- INSERT INTO USERS (username,email,encryptedPAT) VALUES (
---     "aadidapurkar",
---     "aadidapurkar04@gmail.com",
---     "38c30a42a13f13dd114c733296bea1ef:4c96100a247b2148e7087ebf6e6271f88058132aca7c6bfe42ee4b92c36ec7f5ca0865ffa4b352be0a5a1be0402a5249"
--- );
-
--- INSERT INTO SUBSCRIPTIONS (subscriber, repo, username) VALUES 
---     (1, "lockin","aadidapurkar"),
---     (1,"test-notif","aadidapurkar");
-
--- INSERT INTO EVENTS_SUBSCRIPTIONS (subscriptionRef, eventType) VALUES 
---     (1,"PushEvent"),
---     (1,"CreateEvent");
-
--- INSERT INTO EVENTS_SUBSCRIPTIONS (subscriptionRef, eventType) VALUES 
---     (4,"PushEvent"),
---     (4,"CreateEvent");
-
-
-
--- SELECT id, username FROM USERS;
--- SELECT id, subscriber, repo, username FROM SUBSCRIPTIONS;
-
-
--- INSERT INTO SUBSCRIPTIONS (subscriber, repo, username) VALUES 
---     (1,"test-notif","aadidapurkar");
